6c3ffb074c63c191c5503eac314804a3944d8251
Gianna Read-Skelton <giannamread@gmail.com>
2024-11-21 14:44:16 -0700

    added js button listeners

---
+++ index.js
@@ -0,0 +1,12 @@
+opera_button.addEventListener("click", function () {
+  console.log("opera");
+});
+edmonton_button.addEventListener("click", function () {
+  console.log("edmonton");
+});
+calgary_button.addEventListener("click", function () {
+  console.log("calgary");
+});
+intro_button.addEventListener("click", function () {
+  console.log("intro");
+});
