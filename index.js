b47039282d1684edb8ffb2c6cdf671e94afd0418
Gianna Read-Skelton <giannamread@gmail.com>
2024-11-22 10:03:33 -0700

    added media query stylings and sections stylings

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 opera_button.addEventListener("click", function () {
   console.log("opera");
-  intro_section.style.display = "none";
-  opera_section.style.display = "initial";
-  edmonton_section.style.display = "none";
-  calgary_section.style.display = "none";
+  intro_section.classList.add("hide");
+  opera_section.classList.remove("hide");
+  edmonton_section.classList.add("hide");
+  calgary_section.classList.add("hide");
   document.body.classList.add("operaButton");
   document.body.classList.remove("introButton");
   document.body.classList.remove("edmontonButton");
@@ -12,10 +12,10 @@ opera_button.addEventListener("click", function () {
 
 edmonton_button.addEventListener("click", function () {
   console.log("edmonton");
-  intro_section.style.display = "none";
-  opera_section.style.display = "none";
-  edmonton_section.style.display = "initial";
-  calgary_section.style.display = "none";
+  intro_section.classList.add("hide");
+  opera_section.classList.add("hide");
+  edmonton_section.classList.remove("hide");
+  calgary_section.classList.add("hide");
   document.body.classList.add("edmontonButton");
   document.body.classList.remove("introButton");
   document.body.classList.remove("calgaryButton");
@@ -24,10 +24,10 @@ edmonton_button.addEventListener("click", function () {
 
 calgary_button.addEventListener("click", function () {
   console.log("calgary");
-  intro_section.style.display = "none";
-  opera_section.style.display = "none";
-  calgary_section.style.display = "initial";
-  edmonton_section.style.display = "none";
+  intro_section.classList.add("hide");
+  opera_section.classList.add("hide");
+  edmonton_section.classList.add("hide");
+  calgary_section.classList.remove("hide");
   document.body.classList.add("calgaryButton");
   document.body.classList.remove("introButton");
   document.body.classList.remove("edmontonButton");
@@ -36,10 +36,10 @@ calgary_button.addEventListener("click", function () {
 
 intro_button.addEventListener("click", function () {
   console.log("intro");
-  intro_section.style.display = "initial";
-  opera_section.style.display = "none";
-  edmonton_section.style.display = "none";
-  calgary_section.style.display = "none";
+  intro_section.classList.remove("hide");
+  opera_section.classList.add("hide");
+  edmonton_section.classList.add("hide");
+  calgary_section.classList.add("hide");
   document.body.classList.add("introButton");
   document.body.classList.remove("edmontonButton");
   document.body.classList.remove("calgaryButton");
