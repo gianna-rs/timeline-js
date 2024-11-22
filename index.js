d145e10a257e3035b2dcf31f2b7730948c7c5f21
Gianna Read-Skelton <giannamread@gmail.com>
2024-11-21 19:42:24 -0700

    Added JS for content change with buttons

--- index.js
+++ index.js
@@ -1,12 +1,31 @@
 opera_button.addEventListener("click", function () {
   console.log("opera");
+  intro_section.style.display = "none";
+  opera_section.style.display = "initial";
+  edmonton_section.style.display = "none";
+  calgary_section.style.display = "none";
 });
+
 edmonton_button.addEventListener("click", function () {
   console.log("edmonton");
+  intro_section.style.display = "none";
+  opera_section.style.display = "none";
+  edmonton_section.style.display = "initial";
+  calgary_section.style.display = "none";
 });
+
 calgary_button.addEventListener("click", function () {
   console.log("calgary");
+  intro_section.style.display = "none";
+  opera_section.style.display = "none";
+  calgary_section.style.display = "initial";
+  edmonton_section.style.display = "none";
 });
+
 intro_button.addEventListener("click", function () {
   console.log("intro");
+  intro_section.style.display = "initial";
+  opera_section.style.display = "none";
+  edmonton_section.style.display = "none";
+  calgary_section.style.display = "none";
 });
